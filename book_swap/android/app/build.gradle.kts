405d61c8ff1cbac6c327e7a40c43435041a4e50f
pckimlong <pochkimlong88@gmail.com>
2025-04-06 15:30:59 +0700

    fix ndkVersion

--- book_swap/android/app/build.gradle.kts
+++ book_swap/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.sonata.book_swap"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
